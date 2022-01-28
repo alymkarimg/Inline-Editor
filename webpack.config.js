42590156cdec50f91104e67572ebb9c350919e18
Alym Karim <alymkarimg@gmail.com>
2022-01-28 19:16:51 +0000

    test plugin

--- webpack.config.js
+++ webpack.config.js
@@ -46,13 +46,6 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new CKEditorWebpackPlugin( {
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: 'en',
-			additionalLanguages: 'all',
-			allowMultipleJSOutputs: true
-		} ),
 		new webpack.BannerPlugin( {
 			banner: bundler.getLicenseBanner(),
 			raw: true
